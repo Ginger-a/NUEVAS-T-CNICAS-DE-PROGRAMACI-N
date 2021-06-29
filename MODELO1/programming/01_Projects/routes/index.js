3458c547e112f4056910ae87d344d76d0cd405bd
Ginger-a <60004141+Ginger-a@users.noreply.github.com>
2021-06-29 12:27:26 -0500

    middlewares

--- MODELO1/programming/01_Projects/routes/index.js
+++ MODELO1/programming/01_Projects/routes/index.js
@@ -2,16 +2,24 @@
 const { Router } = require('express');
 //Incializamos Router
 const router = Router()
+const {DataValidator} = require('../middlewares/DataValidator')
+const {user} = require("../lib/Schema/User");
 //creacion de los endpoint
 router.get("/", (req,res) => {
-  res.send("Hola mundo con ExpressJs")
+  res.send("Hola mundo con ExpressJS");
 })
-.get("/saludo", (req,res)  => {
-  const { query:{nombre,apellido}} =req;
-  res.json({
-    saludo: `Hola soy ${nombre}  ${apellido}`,
-  });
-}).get('/saludo/:nombre',(req, res) =>{
+.get("/saludo", DataValidator("query",user),(req,res)  => {
+  const {
+    query: { nombre, apellido },
+} = req;
+res.json({
+    saludo: "mundo",
+    nombre: nombre,
+    apellido: apellido,
+    
+});
+})
+.get('/saludo/:nombre',(req, res) =>{
   const {params: {nombre}} = req;
     res.json({
     nombre: nombre
