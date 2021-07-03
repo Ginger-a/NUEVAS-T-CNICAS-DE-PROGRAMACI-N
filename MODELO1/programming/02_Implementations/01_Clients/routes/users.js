a4fee6ddc7bbc39fbfb8add56a793c0bcd44e080
Ginger-a <60004141+Ginger-a@users.noreply.github.com>
2021-07-02 19:23:09 -0500

    Practica

--- MODELO1/programming/02_Implementations/01_Clients/routes/users.js
+++ MODELO1/programming/02_Implementations/01_Clients/routes/users.js
@@ -1,23 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-const {findUsers} = require('../services/Clients.service');
+const { findUsers,createuser } = require('../services/Clients.service');
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  try{
+router.get('/', async function (req, res, next) {
+  try {
     const users = await findUsers()
     res.status(200).json({
-      msg:"Lista de usuarios",
-      body:users,
+      msg: "Lista de usuarios",
+      body: users,
     });
-  } catch (error){
+  } catch (error) {
     console.log(error);
+    res.status(500).json({
+      msg: "Internal server error",
+    });
+
+  }
+})
+  .post("/", async (req, res) => {
+    try {
+      let { body: user } = req;
+      const result = await createuser(user)
+      res.status(200).json({
+        msg: "Usuario creado",
+        body: result,
+      })
+    } catch (error) {
+      console.log(error)
       res.status(500).json({
-        msg:"Internal server error",
-      });
-    
+        msg: "Internal server error",
+      })
     }
-});
+  });
 
 module.exports = router;
--- MODELO1/programming/02_Implementations/01_Clients/services/Clients.service.js
+++ MODELO1/programming/02_Implementations/01_Clients/services/Clients.service.js
@@ -1,24 +1,32 @@
 
-const { MongoConnection } = require ('../lib/mongo');
+const { MongoConnection } = require("../lib/mongo");
 
-//Collection
-const COLLECTION = "clients"
-const findUsers = () => new Promise(async (resolve,reject)=>{
-    try{
-   //inicilazo mongoClient para que me retorno la configuracion de la db
-   const DB = await  MongoConnection();
-   //obtenemos la collection
-   const clients = DB.collection(COLLECTION)
-   const clientsList = await clients.find({}).toArray()
-   resolve(clientsList)
-    } catch (error){
-        reject(error);
+const COLLECTION = "clients";
+const findUsers = () =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const DB = await MongoConnection(); //para que me retorne la configuracion de la db
+      //obtenemops los clientes
+      const clients = DB.collection(COLLECTION);
+      const clientsList = await clients.find({}).toArray();
+      resolve(clientsList);
+    } catch (error) {
+      reject(error);
     }
- 
-
-
-
-});
+  });
+const createuser = (user) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const DB = await MongoConnection();
+      const clients = DB.collection(COLLECTION);
+      const result = await clients.insertOne(user);
+      resolve(result);
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
 module.exports = {
-    findUsers,
+  findUsers,
+  createuser,
 };
